0505d1fd36c1a4102a3ca8aac62b51ee000cb5a4
n3tw0rth <rtheekshanafernando@gmail.com>
2025-07-10 18:19:21 +0530

    feat: draw table

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use registered::registers::{BitSize, RegResult, RegResultGroup, Register, RegisterGroup};
 use std::env;
+use tabled::Table;
 
 use registered::registers::REGISTERS;
 
@@ -12,15 +14,52 @@ fn main() {
         .map(|v| v.strip_prefix("0x").expect("Does not contain 0x prefix"))
         .expect("Might not a valid hex value");
 
-    let registers: String = REGISTERS
+    let registers_group: &RegisterGroup = REGISTERS
         .iter()
-        .map(|v| {
-            print!("{:?}", v);
-            v.join(",")
-        })
-        .collect();
+        .find(|r| r.variants.iter().any(|v| v.name == register_name))
+        .expect("Failed to find the correct register group");
 
     let decimal_value = u64::from_str_radix(str_value, 16);
+    let binary_string = format!("{:0>64b}", decimal_value.unwrap());
 
-    println!("{:?}", registers);
+    let variants = map_values_to_registers(registers_group, binary_string).variants;
+
+    let table = Table::new(variants);
+
+    print!("{table}")
+}
+
+fn map_values_to_registers(
+    register_group: &RegisterGroup,
+    binary_string: String,
+) -> RegResultGroup {
+    let mut variants: Vec<RegResult> = Vec::new();
+
+    register_group.variants.iter().for_each(|r| match r.size {
+        BitSize::Bit8 => {
+            variants.push(process(8, binary_string.clone(), r.clone()));
+        }
+        BitSize::Bit16 => {
+            variants.push(process(16, binary_string.clone(), r.clone()));
+        }
+        BitSize::Bit32 => {
+            variants.push(process(32, binary_string.clone(), r.clone()));
+        }
+        BitSize::Bit64 => {
+            variants.push(process(64, binary_string.clone(), r.clone()));
+        }
+    });
+    RegResultGroup { variants }
+}
+
+fn process(size: usize, binary_string: String, r: Register) -> RegResult {
+    let slice = binary_string.split_at(64 - size).1;
+    let decimal_value = u64::from_str_radix(slice, 2).unwrap_or_default();
+    let hex_value = format!("0x{:x}", decimal_value);
+    RegResult {
+        name: r.name,
+        size: r.size,
+        decimal: decimal_value,
+        hex: hex_value,
+    }
 }
--- src/registers.rs
+++ src/registers.rs
@@ -1,6 +1,83 @@
-pub const REGISTERS: [[&str; 4]; 4] = [
-    ["rax", "eax", "ax", "al"],
-    ["rbx", "ebx", "bx", "bl"],
-    ["rcx", "ecx", "cx", "cl"],
-    ["rdx", "edx", "dx", "dl"],
+use tabled::Tabled;
+
+#[derive(Debug, Clone, Copy, strum_macros::Display, Tabled)]
+pub enum BitSize {
+    Bit64,
+    Bit32,
+    Bit16,
+    Bit8,
+}
+
+#[derive(Debug, Clone, Tabled)]
+pub struct Register {
+    pub name: &'static str,
+    pub size: BitSize,
+}
+
+#[derive(Debug, Clone)]
+pub struct RegisterGroup {
+    pub variants: [Register; 4],
+}
+
+#[derive(Debug, Tabled, Clone)]
+pub struct RegResult {
+    pub name: &'static str,
+    pub size: BitSize,
+    pub decimal: u64,
+    pub hex: String,
+}
+
+#[derive(Debug)]
+pub struct RegResultGroup {
+    pub variants: Vec<RegResult>,
+}
+
+pub const REGISTERS: &[RegisterGroup] = &[
+    RegisterGroup {
+        variants: [
+            Register {
+                name: "rax",
+                size: BitSize::Bit64,
+            },
+            Register {
+                name: "eax",
+                size: BitSize::Bit32,
+            },
+            Register {
+                name: "ax",
+                size: BitSize::Bit16,
+            },
+            Register {
+                name: "al",
+                size: BitSize::Bit8,
+            },
+        ],
+    },
+    RegisterGroup {
+        variants: [
+            Register {
+                name: "rbx",
+                size: BitSize::Bit64,
+            },
+            Register {
+                name: "ebx",
+                size: BitSize::Bit32,
+            },
+            Register {
+                name: "bx",
+                size: BitSize::Bit16,
+            },
+            Register {
+                name: "bl",
+                size: BitSize::Bit8,
+            },
+        ],
+    },
 ];
+
+// pub const REGISTERS: Vec<&str> = [
+//     ["rax", "eax", "ax", "al"],
+//     ["rbx", "ebx", "bx", "bl"],
+//     ["rcx", "ecx", "cx", "cl"],
+//     ["rdx", "edx", "dx", "dl"],
+// ];
